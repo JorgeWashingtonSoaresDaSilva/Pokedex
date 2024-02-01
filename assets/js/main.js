8cb491cd6e16b9bd8b1189a059d6ffb332c08eb1
Jorge Washington Soares da Silva <jwss486@gmail.com>
2024-02-01 19:51:50 -0300

    Criando lista de pokemons com java script

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,37 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
-    response.json().then(function(responseBody){
-        console.log(responseBody)
+function convertPokemonToHtml(pokemon){
+    return
+    `   <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+        `
+            
+}
+
+
+fetch(url)
+    .then((response) => response.json() )
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToHtml(pokemon))
+        }
     })
-})
-.catch(function(error){
-    console.error(error)
-})
-.finally(function(){
-    console.log('Requisição concluida')
-})
+    .catch((error) => console.error)
+    
+
 
