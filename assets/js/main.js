3904b2892db57ab94261d4a89fad55dcd7b7f6e9
Jorge Washington Soares da Silva <jwss486@gmail.com>
2024-02-01 22:24:32 -0300

    convertendo json para uma lista html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,34 +2,34 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHtml(pokemon){
-    return
-    `   <li class="pokemon">
-            <span class="number">#001</span>
-            <span class="name">${pokemon.name}</span>
+function convertPokemonToLi(pokemon) {
+    return `
+         
+             <li class="pokemon">
+                    <><span class="number">#001</span><span class="name">${pokemon.name}</span><div class="detail">
 
-            <div class="detail">
-
-                <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
-                </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-        </li>
-        `
-            
+                     <ol class="types">
+                         <li class="type">grass</li>
+                         <li class="type">poison</li>
+                     </ol>
+                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                         alt="${pokemon.name}">
+                     </></div></>
+            </li>
+          `
+         
 }
 
 
 fetch(url)
     .then((response) => response.json() )
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
+    .then((pokemons) => {
         
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemonToHtml(pokemon))
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            
+            console.log(convertPokemonToLi(pokemon))
         }
     })
     .catch((error) => console.error)
