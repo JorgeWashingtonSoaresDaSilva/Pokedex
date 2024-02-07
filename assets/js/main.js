9ffe66b0081b3f7d5505829f0ca2c2700bd0f77d
Jorge Washington Soares da Silva <jwss486@gmail.com>
2024-02-07 20:51:07 -0300

    criando lista de pokemons dinamicos com java script e laço for usando requisições http

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,23 +6,27 @@ function convertPokemonToLi(pokemon) {
     return `
          
              <li class="pokemon">
-                    <><span class="number">#001</span><span class="name">${pokemon.name}</span><div class="detail">
+                    <span class="number">#001</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
 
-                     <ol class="types">
-                         <li class="type">grass</li>
-                         <li class="type">poison</li>
-                     </ol>
-                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        <ol class="types">
+                             <li class="type">grass</li>
+                             <li class="type">poison</li>
+                        </ol>
+                     
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
                          alt="${pokemon.name}">
-                     </></div></>
+                     </div>
             </li>
           `
          
 }
-console.log(document.getElementById('pokemonList'))
+const pokemonOl = document.getElementById('pokemonList') // aqui eu pego objeto e coloco numa variavel
+// pokemonOl.innerHTML += '<li>Teste</li>'  aqui eu pego lista  e somando mais um item a lista
 
 
-/* fetch(url)
+fetch(url)
     .then((response) => response.json() )
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => {
@@ -30,12 +34,11 @@ console.log(document.getElementById('pokemonList'))
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
             
-            console.log(convertPokemonToLi(pokemon))
-
-            document.getElementsByClassName('pokemos')
+            //console.log(convertPokemonToLi(pokemon))
+            pokemonOl.innerHTML += convertPokemonToLi(pokemon)
         }
     })
-    .catch((error) => console.error) */
+    .catch((error) => console.error) 
     
 
 
