68768f261e0f2d9aaf6e95ffebc1465abef38624
Jorge Washington Soares da Silva <jwss486@gmail.com>
2024-02-12 11:50:55 -0300

    convertendo o modelo de pokeApi para nosso modelo próprio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,31 @@
-//const offset = 0
-//const limit = 10
-//const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-function convertPokemonTypesToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 
 function convertPokemonToLi(pokemon) {
     return `
          
              <li class="pokemon">
-                    <span class="number">#${pokemon.order}</span>
+                    <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
                     <div class="detail">
 
                         <ol class="types">
-                             ${convertPokemonTypesToLi(pokemon.types).join('')}
+                             ${pokemon.types.map((type) =>`<li class="type">${type}</li>`).join('')}
                         </ol>
                      
-                        <img src="${pokemon.sprites.other.dream_world.front_default}"
+                        <img src="${pokemon.photo}"
                          alt="${pokemon.name}">
                      </div>
             </li>
           `
          
 }
-const pokemonOl = document.getElementById('pokemonList') // aqui eu pego objeto e coloco numa variavel
-// pokemonOl.innerHTML += '<li>Teste</li>'  aqui eu pego lista  e somando mais um item a lista
+const pokemonList = document.getElementById('pokemonList') 
 
-// código de consumo de api
 pokeApi.getPokemons().then((pokemons = []) => {
 
-    //const newList = pokemons.map(convertPokemonToLi).join('')
-        //return convertPokemonToLi(pokemon)
-    
-   // const newHtml = newList.join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
 
-    pokemonOl.innerHTML += pokemons.map(convertPokemonToLi).join('')
-        // const listItens = []
-        // for (let i = 0; i < pokemons.length; i++) {
-        //     const pokemon = pokemons[i];
-        //     listItens.push(convertPokemonToLi(pokemon))
-        //     //console.log(convertPokemonToLi(pokemon))
-        //     //pokemonOl.innerHTML += convertPokemonToLi(pokemon)
-        // }
-       //console.log(listItens)
+    pokemonList.innerHTML = newHtml
+        
     })
     .catch((error) => console.error) 
     
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,25 @@
 // criando objeto
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokemonDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonDetail.order
+    pokemon.name = pokemonDetail.name
+
+    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) =>{
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then((convertPokeApiDetailToPokemon))
 }
 // criando aero funtion
 pokeApi.getPokemons = (offset = 0,limit = 5) => {
@@ -10,7 +27,7 @@ pokeApi.getPokemons = (offset = 0,limit = 5) => {
     return fetch(url)
         .then((response) => response.json() )
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map((pokeApi.getPokemonDetail)))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequest) => Promise.all(detailRequest))
         .then((pokemonsDetails) => pokemonsDetails)
         //.catch((erro) => console.error(console.error))
