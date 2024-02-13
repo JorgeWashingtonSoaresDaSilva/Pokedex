e8e88ced16574217d24ca5fde6093a030f653fa5
Jorge Washington Soares da Silva <jwss486@gmail.com>
2024-02-12 22:58:20 -0300

    usando javaScript para paginar a busca de pokemons e limitar as geração

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,48 @@
 const pokemonList = document.getElementById('pokemonList') 
 const loadMoreButton = document.getElementById('loadMoreButton')
-function convertPokemonToLi(pokemon) {
-    return `
-         
-             <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+const maxRecords = 151;
+const limit = 5;
+let offset = 0;
 
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
+function loadPokemonItens(offset,limit){
+  
+    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>`
+             
+        <li class="pokemon ${pokemon.type}">
+           <span class="number">#${pokemon.number}</span>
+           <span class="name">${pokemon.name}</span>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+           <div class="detail">
+               <ol class="types">
+                   ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+               </ol>
+
+           <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+             </div>
         </li>
-          `
-         
+     `).join('')
+        pokemonList.innerHTML += newHtml
+            
+        })
+        .catch((error) => console.error) 
 }
+loadPokemonItens(offset, limit)
+loadMoreButton.addEventListener('click',() =>{
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
 
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-
-    pokemonList.innerHTML = newHtml
-        
-    })
-    .catch((error) => console.error) 
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+    
     
 
 
